1ddb394bbe884021699b60d76dabef92bb16d5db
YuXiangWeng <yuxiangw4@nycstudents.net>
2024-10-01 15:46:32 +0000

    Added match

--- Main.java
+++ Main.java
@@ -15,9 +15,26 @@ public class Main
         game.recordPlay(3);
         info = game.getScore();
         System.out.println(info);
-
-
-        
+        game.recordPlay(1);
+        info = game.getScore();
+        System.out.println(info);
+        game.recordPlay(0);
+        info = game.getScore();
+        System.out.println(info);
+        game.recordPlay(0);
+        info = game.getScore();
+        System.out.println(info);
+        game.recordPlay(4);
+        info = game.getScore();
+        System.out.println(info);
+        game.recordPlay(0);
+        info = game.getScore();
+        System.out.println(info);
+        Scoreboard match = new Scoreboard("Lions", "Tiger");
+        info = match.getScore();
+        System.out.println(info);
+        info = game.getScore();
+        System.out.println(info);
     }
     
 
